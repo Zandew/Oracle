4e503dd75b808c448291c7741346ee25c3131f9a
Andrew Xue <andewzxue@gmail.com>
2020-07-08 16:46:45 -0400

    Initial Commit

---
+++ MiniMusicWidget/AppDelegate.swift
@@ -0,0 +1,26 @@
+//
+//  AppDelegate.swift
+//  MiniMusicWidget
+//
+//  Created by Andrew Xue on 2020-07-08.
+//  Copyright © 2020 Andrew Xue. All rights reserved.
+//
+
+import Cocoa
+
+@NSApplicationMain
+class AppDelegate: NSObject, NSApplicationDelegate {
+
+
+
+    func applicationDidFinishLaunching(_ aNotification: Notification) {
+        // Insert code here to initialize your application
+    }
+
+    func applicationWillTerminate(_ aNotification: Notification) {
+        // Insert code here to tear down your application
+    }
+
+
+}
+
